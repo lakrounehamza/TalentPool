0064d59b326eb1b3652b456186ea59036b289d55
lakrounehamza <lakrounehamza10@gmail.com>
2025-03-26 16:22:15 +0100

    fix:Controller  de  recruteur

--- app/Http/Controllers/Api/RecruteurController.php
+++ app/Http/Controllers/Api/RecruteurController.php
@@ -3,32 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use App\Models\Candidate;
-use App\Models\Annonce;
-use App\Models\Candidature;
-use Illuminate\Support\Facades\Auth;
 
 class RecruteurController extends Controller
 {
     public function recruteur() 
     { 
-        $id = Auth::guard('api')->user()->id;
-
-        $annonces = Annonce::where('recruteur_id', $id)->count();
-
-        $candidatures = Candidature::join('annonces', 'annonces.id', '=', 'candidatures.annonce_id')
-            ->where('annonces.recruteur_id', $id)
-            ->count();
-        $candidates = Candidate::join('candidatures', 'candidates.id', '=', 'candidatures.candidate_id')
-            ->join('annonces', 'annonces.id', '=', 'candidatures.annonce_id')
-            ->where('annonces.recruteur_id', $id)
-            ->count();
-
-        return response()->json([
-            'annonces' => $annonces,
-            'candidatures' => $candidatures,
-            'candidates' => $candidates
-        ]);
+        
     }
 }
---
+++ app/repositories/RecruteurRepositorie.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Http\Request;
+use App\Models\Candidate;
+use App\Models\Annonce;
+use App\Models\Candidature;
+use Illuminate\Support\Facades\Auth;
+
+class RecruteurRepositorie
+{
+  public function recruteur()
+  {
+    $id = Auth::guard('api')->user()->id;
+
+    $annonces = Annonce::where('recruteur_id', $id)->count();
+
+    $candidatures = Candidature::join('annonces', 'annonces.id', '=', 'candidatures.annonce_id')
+        ->where('annonces.recruteur_id', $id)
+        ->count();
+    $candidates = Candidate::join('candidatures', 'candidates.id', '=', 'candidatures.candidate_id')
+        ->join('annonces', 'annonces.id', '=', 'candidatures.annonce_id')
+        ->where('annonces.recruteur_id', $id)
+        ->count();
+
+
+    $data = [
+      'candidates' => $candidates,
+      'annonces' => $annonces,
+      'candidatures' => $candidatures,
+    ];
+    return $data;
+  }
+}
